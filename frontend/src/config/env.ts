80bbb4315476fba7b30f34df9b45c06cb9a421e0
Tchinde Fosso <tchindefossomael@yahoo.fr>
2026-09-06 23:03:57 +0200

    update /frontend/src/config/env.ts

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,3 +1,16 @@
+// src/config/env.ts
+
+const getApiUrl = () => {
+  let url = process.env.NEXT_PUBLIC_API_URL || "https://backend-production-b4b03.up.railway.app/api";
+  
+  // when the url does not start with http:// or https://, prepend https:// to it
+  //if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    //url = `https://${url}`;
+  //}
+  
+  return url;
+};
+
 export const ENV = {
-  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  API_URL: getApiUrl(),
 } as const;
